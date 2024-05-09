6db3961e5c575a56b9afa7524ab6dcb12bf46ab2
hoangnguyen13567 <hoang2004xyz@gmail.com>
2024-05-09 17:32:23 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,16 @@
+<style>
+    p {
+        font-size: 20px;
+        font-weight: 400;
+        margin-top: 20px;
+    }
+</style>
+<section class="py-5">
+    <div class="container about">
+        <div class="card rounded-0">
+            <div class="card-body">
+                <?php include "about.html" ?>
+            </div>
+        </div>
+    </div>
+</section>
